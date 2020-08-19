0560d590bfdb50629aeb1fd1eb4d5e7a995a784c
Oluwatobi Bello <bellotobi10@gmail.com>
2020-08-19 19:24:18 -0400

    Ability for the admin to add new categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,26 @@
+class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
+  def index
+    @products = Product.group(:category).all
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(name: params[:category][:name])
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: "Category created!"
+    else
+      render new
+    end
+  end
+
+  
+
+end
